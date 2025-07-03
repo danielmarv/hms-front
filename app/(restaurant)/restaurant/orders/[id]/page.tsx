75db9dc9959552385a9dad01ad15867d00840bf8
Daniel Ntege <danientege785@gmail.com>
2025-07-04 00:00:29 +0300

    commit

--- app/(restaurant)/restaurant/orders/[id]/page.tsx
+++ app/(restaurant)/restaurant/orders/[id]/page.tsx
@@ -302,7 +302,6 @@ export default function RestaurantOrderDetailPage() {
       </div>
 
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-        {/* Main Content */}
         <div className="lg:col-span-2 space-y-6">
           {/* Order Details */}
           <Card>
