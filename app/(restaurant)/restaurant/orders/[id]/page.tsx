1554775194846d070b3647393f33f0ff8cecb0fe
Daniel Ntege <danientege785@gmail.com>
2025-07-04 00:00:57 +0300

    commit

--- app/(restaurant)/restaurant/orders/[id]/page.tsx
+++ app/(restaurant)/restaurant/orders/[id]/page.tsx
@@ -341,7 +341,6 @@ export default function RestaurantOrderDetailPage() {
                 )}
               </div>
 
-              {/* Customer Information */}
               {(order.customerName || order.customerPhone || order.customerEmail) && (
                 <>
                   <Separator />
